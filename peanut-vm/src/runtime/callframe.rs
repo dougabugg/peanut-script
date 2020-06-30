b850c2813b2cfc75c6db4c1fcee15d12c05a4f8b
doug <hello@dougs.page>
2020-06-30 01:18:10 -0500

    started work on runtime::vm

--- peanut-vm/src/runtime/callframe.rs
+++ peanut-vm/src/runtime/callframe.rs
@@ -9,6 +9,17 @@ pub struct CallFrame {
 }
 
 impl CallFrame {
+    pub fn new(function: Function) -> CallFrame {
+        let mut stack = Vec::new();
+        stack.resize_with(function.stack_size as usize, || Value::None);
+        CallFrame {
+            parent: None,
+            function,
+            cursor: 0,
+            stack
+        }
+    }
+
     pub fn load(&self, index: usize) -> Result<&Value, OpError> {
         self.stack.get(index).ok_or(OpError::StackRead(index))
     }
--- peanut-vm/src/runtime/vm.rs
+++ peanut-vm/src/runtime/vm.rs
@@ -1,1 +1,21 @@
+use crate::bytecode::OpError;
+use crate::datamodel::Function;
 
+use super::CallFrame;
+
+pub struct VirtualMachine {
+    frame: Box<CallFrame>,
+}
+
+impl VirtualMachine {
+    pub fn new(func: Function) -> VirtualMachine {
+        VirtualMachine {
+            frame: Box::new(CallFrame::new(func))
+        }
+    }
+
+    pub fn step(&mut self) -> Result<(), OpError> {
+        // TODO stopped right here
+        panic!();
+    }
+}
