4988b1834262ba5d444c0b54cc464b83281d48a8
doug <hello@dougs.page>
2020-07-10 12:22:23 -0500

    impl code generation for Expr variants

--- compiler/src/stage0-new/expr.rs
+++ compiler/src/stage0-new/expr.rs
@@ -46,6 +46,79 @@ pub enum Expr {
 }
 
 impl Expr {
+    pub fn compile(&self, g: &mut CodeGenerator) {
+        match self {
+            Expr::LiteralValue(literal) => g.push(ops::LiteralCreate::new(*literal).into()),
+            Expr::Var(var) => g.push_var_load(var.inner),
+            Expr::ModuleRef => g.push(ops::StackLoad::new(0).into()),
+            Expr::BinaryOp(b) => b.compile(g),
+            Expr::UnaryOp(u) => u.compile(g),
+            Expr::Call { func, args } => {
+                func.compile(g);
+                assert!(args.len() <= 255);
+                for arg in args {
+                    arg.compile(g);
+                }
+                g.push(ops::Call::new(args.len() as u8).into());
+            }
+            Expr::SeqIndex { seq, index } => {
+                seq.compile(g);
+                index.compile(g);
+                g.push(ops::SeqGet.into());
+            }
+            Expr::SeqLen { seq } => {
+                seq.compile(g);
+                g.push(ops::SeqLen.into());
+            }
+            Expr::SeqToList { seq } => {
+                seq.compile(g);
+                g.push(ops::SeqToList.into());
+            }
+            Expr::TupleCreate(items) => {
+                assert!(items.len() <= 255);
+                for item in items {
+                    item.compile(g);
+                }
+                g.push(ops::TupleCreate::new(items.len() as u8).into());
+            }
+            Expr::TupleFromList(e) => {
+                e.compile(g);
+                g.push(ops::TupleFromList.into());
+            }
+            Expr::TableCreate(e) => {
+                e.compile(g);
+                g.push(ops::TableCreate.into());
+            }
+            Expr::ListCreate(items) => {
+                assert!(items.len() <= 255);
+                for item in items {
+                    item.compile(g);
+                }
+                g.push(ops::ListCreate::new(items.len() as u8).into());
+            }
+            Expr::ListGetSlice { list, a, b } => {
+                list.compile(g);
+                a.compile(g);
+                b.compile(g);
+                g.push(ops::ListGetSlice.into());
+            }
+            Expr::ListPop(e) => {
+                e.compile(g);
+                g.push(ops::ListPop.into());
+            }
+            Expr::BufferCreate(e) => {
+                e.compile(g);
+                g.push(ops::BufferCreate.into());
+            }
+            Expr::BufferGetSlice { buffer, a, b } => {
+                buffer.compile(g);
+                a.compile(g);
+                b.compile(g);
+                g.push(ops::BufferGetSlice.into());
+            }
+        }
+    }
+
     pub fn find_vars(&self) -> Vec<Span<Var>> {
         let mut vars = Vec::new();
         self.acc_vars(&mut vars);
@@ -100,27 +173,4 @@ impl Expr {
             },
         }
     }
-}
-
-pub struct BinaryOp {
-    pub op_type: BinaryOpType,
-    pub lhs: Box<Expr>,
-    pub rhs: Box<Expr>,
-}
-
-#[rustfmt::skip]
-pub enum BinaryOpType {
-    Add, Sub, Mul, Div, Rem, Shl, Shr, And, Or, Xor,
-    Equal, NotEqual, Greater, GreaterOrEqual, Less, LessOrEqual,
-    Identity, LogicAnd, LogicOr
-}
-
-pub struct UnaryOp {
-    pub op_type: UnaryOpType,
-    pub expr: Box<Expr>,
-}
-
-#[rustfmt::skip]
-pub enum UnaryOpType {
-    Neg, Not, LogicNot, IntToReal, Floor, Ceil, Trunc, Round
 }
