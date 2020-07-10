050fd51ae5889b54e2af58bdb4ca65731e9938d7
doug <hello@dougs.page>
2020-07-10 18:41:33 -0500

    initial work on type checker for stage1 compiler

--- compiler/src/lib.rs
+++ compiler/src/lib.rs
@@ -1,41 +1,15 @@
 extern crate peanut_script_vm as vm;
 
 pub mod stage0;
+pub mod stage1;
 
 /*
-STAGE 0
-the stage0 AST doesn't do any type checking and is used to generate bytecode
-for the VM to execute.
+The compiler has many stages, which execute in descending order.
 
-STAGE 1
-the stage1 AST will have type checking and will generate a stage0 AST. the stage1
-AST won't support user-defined types, but will check for any type errors that would
-cause the program to crash when executing in the VM. this will also handle loop
-labels, assigning indices to local variables, and handling constant expressions
-and string literals.
+    Stage 0 focuses entirely on bytecode generation. It doesn't check the
+types of expressions, and provides only a few basic language abstractions
+on top of the bytecode operations, like loops and if-else statements.
 
-STAGE 2
-the stage2 AST will implement the user-facing type system and generate a stage1 AST.
-the type system consists of: associated functions, run-time type information,
-traits (interfaces), dynamic dispatch, and tagged unions.
-*/
-
-/*
-ISSUES:
-- if stage2 already does type checking, isn't it redundant for stage1 to also do
-    type checking? should we skip implementing type checking in stage1?
-
-currently, stage0 is very low-level, with the AST layout require special handling
-(see notes in src/stage0/mod.rs).
-
-I think we might want to re-write the stage0 AST, and implement some of the features
-from stage1 into it, like variable indexing and loop labeling.
-*/
-
-/////////////////////////
-
-/*
-here's the new plan:
-- refactor stage0, and allow it to share definitions with stage1
-- stage1 will be identical to stage0, except for variable indexing and loop labeling.
+    Stage 1 is currently in development. It will perform type checking, and
+will convert the syntax tree for input into the stage 0 compiler.
 */
